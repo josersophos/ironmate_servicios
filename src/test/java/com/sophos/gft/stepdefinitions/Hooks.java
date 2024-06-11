1138e3e3ee660fe287eb64492d82d22b4677bdbe
Jose Rivera <joser@backbase.com>
2024-06-11 18:11:16 -0500

    Add missing classes

---
+++ src/test/java/com/sophos/gft/stepdefinitions/Hooks.java
@@ -0,0 +1,13 @@
+package com.sophos.gft.stepdefinitions;
+
+import io.cucumber.java.Before;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+public class Hooks {
+
+    @Before
+    public void prepareStage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+}
